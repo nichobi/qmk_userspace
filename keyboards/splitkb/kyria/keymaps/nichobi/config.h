b0978f8e2ebc1129a5a5ecaebc7e9305ab2ea23f
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2024-09-27 17:44:46 +0200

    kyria: Configure mouse acceleration

--- keyboards/splitkb/kyria/keymaps/nichobi/config.h
+++ keyboards/splitkb/kyria/keymaps/nichobi/config.h
@@ -1,20 +1,11 @@
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    ifndef RGBLIGHT_LIMIT_VAL
-#    	define RGBLIGHT_LIMIT_VAL 150
-#    endif
-#endif
+#define MK_KINETIC_SPEED            // Enable kinetic mode
+#define MOUSEKEY_INITIAL_SPEED 300  // Initial speed of the cursor in pixel per second
+#define MOUSEKEY_BASE_SPEED    2000 // Maximum cursor speed at which acceleration stops
+#define MOUSEKEY_INTERVAL      5    // Time between cursor movements in milliseconds
+
+#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS     1 // Initial number of movements of the mouse wheel
+#define MOUSEKEY_WHEEL_BASE_MOVEMENTS        32 // Maximum number of movements at which acceleration stops
+#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48 // Accelerated wheel movements
+#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8  // Decelerated wheel movements
