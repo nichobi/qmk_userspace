7e3999df3312f6590f64c3ad4dab95aa714757fb
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2024-09-27 16:33:29 +0200

    kyria: Add modified OLED program

--- keyboards/splitkb/kyria/keymaps/nichobi/keymap.c
+++ keyboards/splitkb/kyria/keymaps/nichobi/keymap.c
@@ -208,3 +208,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  //                        └───────┴───────┴───────┴───────┴───────┘  └───────┴───────┴───────┴───────┴───────┘
 //     ),
 };
+
+#ifdef OLED_ENABLE
+#include "oled.c"
+#endif
